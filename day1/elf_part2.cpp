3978a7e21d08e221b9053d3ad20a8ef08eab2e5d
Kanykei Tashbaeva <tashbaevakanykei@gmail.com>
2022-12-02 11:51:59 +0100

    day1

--- day1/elf_part2.cpp
+++ day1/elf_part2.cpp
@@ -1,30 +1,32 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 int	main(int argc, char **argv)
 {
-	std::ifstream	file(argv[1]);
-	std::string		line;
-	int				cal = 0;
-	int				one = 0;
-	int				two = 0;
-	int				three = 0;
+	std::ifstream		file(argv[1]);
+	std::string			line;
+	std::vector<long>	v;
+	int					cal = 0;
+	long				sum = 0;
 
 	if (file.is_open()) {
 		while (file.good()) {
-			while (std::getline(file, line) && line.length() > 1 && line.compare("\n") != 0)
+			while (std::getline(file, line) && line.length() > 2 && line.compare("\n") != 0)
 				cal += stoi(line);
-			one = std::max(cal, one);
-			two = std::max(two, one);
-			three = std::max(three, two);
-			cal = 0;
-			one = 0;
+			v.push_back(cal);
 			if (file.eof())
 				break;
 		}
-		std::cout << "Result is " << one + two + three << std::endl;
 		file.close();
 	}
+	std::sort(v.begin(), v.end());
+	for (std::vector<long>::reverse_iterator it = v.rbegin(); it != v.rbegin() + 3; ++it)
+	{
+		std::cout << "top calories: " << *it << std::endl;
+		sum += *it;
+	}
+	std::cout << "Result is " << sum << std::endl;
 	return 0;
 }

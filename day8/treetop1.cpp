67e21f39ee914cd862cb53b1296e5fa5f2a47d28
Kanykei Tashbaeva <tashbaevakanykei@gmail.com>
2023-01-13 22:23:22 +0100

    day8 buggy

--- day8/treetop1.cpp
+++ day8/treetop1.cpp
@@ -5,22 +5,6 @@
 #include <algorithm>
 using namespace std;
 
-int	scanner(vector<vector<char> > &arr)
-{
-	int	dist;
-	for (int i = 0; i < arr.size(); i++) {
-		vector<char>::iterator max_it = max_element(arr[i].begin(), arr[i].end());
-		for (vector<char>::iterator it = arr[i].begin(); it != arr[i].end(); ++it) {
-			if (max_it < it)
-				dist = it - max_it;
-			else
-				dist = max_it - it;
-		}
-	}
-
-
-}
-
 vector<char> split_line(string const &str) { 
 	vector<char> result(str.length()); 
 	copy(begin(str), end(str), begin(result)); 
@@ -29,11 +13,10 @@ vector<char> split_line(string const &str) {
 
 int	main(int argc, char **argv)
 {
-	ifstream	file(argv[1]);
-	string		line;
+	ifstream				file(argv[1]);
+	string					line;
 	vector<vector<char> >	arr;
-	vector<char>	temp;
-	int			result;
+	int						sum = 0;
 
 	while (file.is_open() && file.good())
 	{
@@ -41,7 +24,47 @@ int	main(int argc, char **argv)
 		arr.push_back(split_line(line));
 	}
 	file.close();
-	result = scanner(arr);
-	cout << "Result is " << result << endl;
+	int row = arr.size();
+	int col = arr[0].size();
+	char current = 0;
+	for (int i = 1; i < row - 1; i++) {
+		for (int j = 1; j < col - 1; j++) {
+			bool left =  true;
+			bool right =  true;
+			current = arr[i][j];
+			for (int c = 0; c < j; c++) {
+				if (arr[i][c] >= current) {
+					left = false;
+					break;
+				}
+			}
+			for (int c = j + 1; c < col; c++) {
+				if (arr[i][c] >= current) {
+					right = false;
+					break;
+				}
+			}
+			if (left || right) {
+				sum++;
+				continue;
+			}
+			for (int r = 0; r < i; r++) {
+				if (arr[r][j] >= current) {
+					left = false;
+					break;
+				}
+			}
+			for (int r = i + 1; r < row; r++) {
+				if (arr[r][j] >= current) {
+					right = false;
+					break;
+				}
+			}
+			if (left || right)
+				sum++;
+		}
+	}
+	sum += (row * 4) - 4;
+	cout << "Result is " << sum << endl;
 	return 0;
 }

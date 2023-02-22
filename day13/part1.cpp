9dd675c1d5bfb89e8b3fa083666832189a4a20f6
smkatash <tashbaevakanykei@gmail.com>
2023-02-22 21:50:44 +0100

    d13 pt.2

--- day13/part1.cpp
+++ day13/part1.cpp
@@ -1,52 +1,76 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-void	remove_symbols(string *str) {
-	(*str).erase(remove((*str).begin(), (*str).end(), '['), (*str).end());
-	(*str).erase(remove((*str).begin(), (*str).end(), ']'), (*str).end());
-	(*str).erase(remove((*str).begin(), (*str).end(), ','), (*str).end());
-}
-
-int count_brackets(string str) {
-	return count(str.begin(), str.end(), '[');
-}
-
 int	compare_pair(string pair1, string pair2) {
-	int c1 = count_brackets(pair1);
-	int c2 = count_brackets(pair2);
-	remove_symbols(&pair1);
-	remove_symbols(&pair2);
-	int	min_len = min(pair1.length(), pair2.length());
-	int min_pair;
-
-	if (pair1.empty() && pair2.empty()) {
-		if (c1 < c2)
+	int first = 0;
+	int second = 0;
+	int i = 0;
+	int j = 0;
+	while (!pair1.empty() && !pair2.empty()){
+		if (isdigit(pair1[i]) && isdigit(pair2[j])) {
+			while (isdigit(pair1[i])) {
+				first = first * 10 + (pair1[i] - '0');
+				i++;
+			}
+			while (isdigit(pair2[j])) {
+				second = second * 10 + (pair2[j] - '0');
+				j++;
+			}
+			if (first < second) {
+				return 1;
+			}
+			else if (first > second) {
+				return 2;
+			}
+			else {
+				pair1.erase(0, i);
+				pair2.erase(0, j);
+				return compare_pair(pair1, pair2);
+			}
+		}
+		else if (pair1[i] == pair2[j]) {
+			i++;
+			j++;
+			pair1.erase(0, i);
+			pair2.erase(0, j);
+			return compare_pair(pair1, pair2);
+		} 
+		else if (pair1[i] == ']') {
 			return 1;
-		else
+		}
+		else if (pair2[j] == ']') {
 			return 2;
+		}
+		else if (isdigit(pair1[i]) && pair2[j] == '[') {
+			while (isdigit(pair1[i])) {
+				first = first * 10 + (pair1[i] - '0');
+				i++;
+			}
+			pair1.insert(0, 1, '[');
+			pair1.insert(1 + i, 1, ']');
+			return compare_pair(pair1, pair2);
+		}
+		else if (isdigit(pair2[j]) && pair1[i] == '[') {
+			while (isdigit(pair2[j])) {
+				first = first * 10 + (pair2[j] - '0');
+				j++;
+			}
+			pair2.insert(0, 1, '[');
+			pair2.insert(1 + j, 1, ']');
+			return compare_pair(pair1, pair2);
+		}
 	}
-	cout << endl << pair1 << " | " << pair2 << endl;
-	if (pair1.empty() && !pair2.empty())
+	if (pair1.empty())
 		return 1;
-	else if (!pair1.empty() && pair2.empty())
+	else if (pair2.empty())
 		return 2;
-
-	for (int i = 0; i < min_len; i++) {
-		if (pair1[i] == pair2[i])
-			continue;
-		if (pair1[i] < pair2[i])
-			return 1;
-		else if (pair1[i] > pair2[i])
-			return 2;
-	}
-	if (pair1.length() < pair2.length()) {
+	else
 		return 1;
-	}
-	else if (pair1.length() > pair2.length())
-		return 2;
-	return -1;
 }
 
 int main(int argc, char** argv) {
@@ -54,22 +78,19 @@ int main(int argc, char** argv) {
 	string		pair1;
 	string		pair2;
 	int			counter = 0;
-	int			p1 = 0;
-	int			p2;
+	int			indx = 1;
 	int			min = 0;
 	
 	while (file.is_open() && file.good()) {
 		file >> pair1;
 		file >> pair2;
-		p1++;
-		// cout << "index " << p1 << " " << p2 << endl;
 		min = compare_pair(pair1, pair2);
 		if (min) {
 			if (min == 1) {
-				counter += p1;
-				cout << p1 << endl;
+				counter += indx;
 			}
 		}
+		indx++;
 	}
 	cout << "Result is " << counter << endl;
 	return 0;

73dad4505328a74f38cadae0445ac7ce9c89d835
smkatash <tashbaevakanykei@gmail.com>
2025-12-01 23:31:20 +0100

    2025

---
+++ 2025/day1/part2/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+var counter int
+var sum int
+
+func rotateLeft(steps int) {
+	if counter-steps < 0 {
+		if counter != 0 {
+			sum += 1
+		}
+		counter = 100 - (steps - counter)
+		if counter < 0 {
+			counter = 100 + counter
+		}
+	} else if counter == 0 {
+		counter = 99 - (steps - 1)
+		if counter < 0 {
+			counter = 100 + counter
+		}
+	} else {
+		counter -= steps
+	}
+}
+
+func rotateRight(steps int) {
+	if (counter + steps) >= 100 {
+		counter = (counter + steps) - 100
+		if counter != 0 {
+			sum += 1
+		}
+	} else {
+		counter += steps
+	}
+}
+
+func main() {	
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	counter = 50
+	sum = 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		direction := line[0:1]
+		stepsString := line[1:]
+		steps, err := strconv.Atoi(stepsString)
+		if (steps > 100) {
+			sum += steps / 100
+			steps = steps % 100
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch direction {
+		case "L":
+			rotateLeft(steps)
+		case "R":
+			rotateRight(steps)
+		default:
+			log.Fatal("Unknown direction")
+		}
+		if (counter == 0) {
+			sum += 1
+		}
+	}
+
+	fmt.Println("Sum: ", sum)
+}
